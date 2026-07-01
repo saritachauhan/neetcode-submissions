36df066b243d11dcebaaf87b34740af618e366a3
Sarita Chauhan <49169165+saritachauhan@users.noreply.github.com>
2026-07-01 06:39:53 +0530

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currSum = 0;
+        int sum = Integer.MIN_VALUE;
+  
+        for (int i = 0; i < nums.length; i++) {
+            currSum+=nums[i];
+            sum=Math.max(sum, currSum);
+            if(currSum<0){
+                currSum=0;
+            }
+            
+        }
+        return sum;
+    }
+}
